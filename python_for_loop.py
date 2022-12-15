0ef6f803df0bb6bae4029fe4de0e9cf788ef7828
Sunny7372 <sunnymishra7372@gmail.com>
2022-12-15 08:21:39 -0800

    commited by sunny

---
+++ python_for_loop.py
@@ -0,0 +1,20 @@
+# write a code to print numbers from 1 to 10
+for num in range(1,11):
+    print(num)
+
+# # write a code to print numbers from 1 to 10 using 2 steps
+for num in range(1,11,2):
+     print(num) 
+
+# write a program to print numbers from 10 to 1
+for num in range(10,0,-1):
+    print(num)
+
+# write a program to calculate the sum of given list elements using for loop
+# output = 25
+int_list = [4,8,-2,10,5]
+sum=0
+for num in int_list:
+    sum = sum+num
+print(" total sum is",sum)
+
