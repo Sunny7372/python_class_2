4f629d34c3b0ca8294e14fff4448e3a200877fb8
Sunny7372 <sunnymishra7372@gmail.com>
2022-12-15 08:26:17 -0800

    commited by sunny

---
+++ python_while_loop.py
@@ -0,0 +1,11 @@
+# write a program to print the table of 9
+num = 9
+counter = 1
+while counter<=10:
+    ans = num*counter
+    print(num, '*' ,counter ,'=' ,ans)
+    counter = counter+1
+
+while True:
+    print("sunny")
+  
