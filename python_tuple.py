1788a8c5b3bbb68687bd7352e8b83a546967f7eb
Sunny7372 <sunnymishra7372@gmail.com>
2022-12-15 08:36:00 -0800

    commited by sunny

---
+++ python_tuple.py
@@ -0,0 +1,18 @@
+# work with tuple
+# empty tuple
+t1 = ()
+# failed assignment and append because it is immutable
+#t1.append(5)
+#t1[0] =3
+
+t2 =(20,30,40,50)
+print(len(t2))
+
+# indexing also works  for tuple
+# access 3rd element from tuple
+
+print(t2[2])
+
+#  indexing works 
+# slicing works
+# iterations also works
